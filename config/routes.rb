8d2b6d17c0ca52e6938020eecce8827851f74df4
Thooams <thooams@gmail.com>
2015-11-07 18:26:56 +0100

    [...work in progress...]

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,7 @@ Rails.application.routes.draw do
 
   resources :users
   get 'home/index'
-  #get 'components/index'
+  get 'components' => 'components#index'
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
