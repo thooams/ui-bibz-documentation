520145aa8f2b1f709d9960d9f14e5719b136912c
Jeremy <jgontard@codde.fr>
2016-02-25 12:21:44 +0100

    Add beautiful home page

--- config/routes.rb
+++ config/routes.rb
@@ -5,14 +5,14 @@ Rails.application.routes.draw do
   resources :users
   get 'home/index'
   get 'getting_started' => 'home#getting_started'
-  get 'about'       => 'home#about'
-  get 'components'  => 'components#index'
+  get 'about'           => 'home#about'
+  get 'components'      => 'components#index'
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  root 'home#getting_started'
+  root 'home#index'
 
   #resource :components, only: :index do
     #resource :buttons
