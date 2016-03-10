d41999b3432386a3614ff8b7fd2830b8a4b68a05
Jeremy <jgontard@codde.fr>
2016-03-10 11:17:02 +0100

    Little fix, don't need to talk about it again

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,5 @@
 class ApplicationController < ActionController::Base
-  layout "applicationrightnav"
+  layout "application_right_nav"
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
