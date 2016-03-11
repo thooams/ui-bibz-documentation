b9e52770ec78a17aa8097874d8e336c247ae78f9
Jeremy <jgontard@codde.fr>
2016-03-11 14:30:08 +0100

    Remove commented line useless

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -13,29 +13,13 @@
 //= require ui_bibz
 //= require turbolinks
 
-var scrollBottom = $(window).scrollTop() + $(window).height(); // Valeur de retour absurde => inutilisable
-// Test height
-//alert($(window).height());
-// methode top() not found ...
-//alert($('footer').offset().top);
-
-//var offset = $("footer").offset().left;
-
 // Fonction pour fixer le menu de droite lors du scrolling
 $(window).scroll(function() {
 	if ($(this).scrollTop() > 370) {
-    //$('.left-nav').addClass('fix-left-nav');
 		$('.secondary-nav').addClass('fix-secondary-nav');
 		$('.content').addClass('fix-content');
   } else {
-    //$('.left-nav').removeClass('fix-left-nav');
 		$('.secondary-nav').removeClass('fix-secondary-nav');
 		$('.content').removeClass('fix-content');
   }
-
-	// Non fonctionnel
-	// Trouver la hauteur du footer pour fixer le menu de gauche (left-nav) lorsque le scroll arrive au footer
-	/*if ($(this).scrollBottom > 0) {
-		$('.left-nav').removeClass('fix-left-nav');
-	}*/
 });
