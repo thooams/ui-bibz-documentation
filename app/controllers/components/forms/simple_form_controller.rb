ac8277bce5bcde10ce5b98b1bf0f9e8f71fcf745
Thomas HUMMEL <thomas@hummel.link>
2017-10-24 15:18:21 +0200

    add simple form inputs

--- app/controllers/components/forms/simple_form_controller.rb
+++ app/controllers/components/forms/simple_form_controller.rb
@@ -1,7 +1,14 @@
 class Components::Forms::SimpleFormController < Components::FormsController
 
   def inputs
-    @user= User.first
+    @user = User.new params[:user].nil? ? {} : inputs_params
+    ap params
+    ap inputs_params
+    ap @user
+  end
+
+  def inputs_params
+    params.require(:user).permit(:name, :country_id, :price, :company, :price_formula, email: [])
   end
 
 end
