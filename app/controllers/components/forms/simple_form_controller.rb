a354345a9ae5cb5a34ec612b3f5fbdc394d954b3
Thomas HUMMEL <thomas@hummel.link>
2017-10-24 15:53:09 +0200

    fix switch field page

--- app/controllers/components/forms/simple_form_controller.rb
+++ app/controllers/components/forms/simple_form_controller.rb
@@ -2,9 +2,6 @@ class Components::Forms::SimpleFormController < Components::FormsController
 
   def inputs
     @user = User.new params[:user].nil? ? {} : inputs_params
-    ap params
-    ap inputs_params
-    ap @user
   end
 
   def inputs_params
