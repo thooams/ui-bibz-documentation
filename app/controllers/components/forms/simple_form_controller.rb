4a907188d360f9463d43df985a8e1bc9017ed8d4
Thomas HUMMEL <thomas@hummel.link>
2017-10-26 16:59:47 +0200

    fix navbar with simple form

--- app/controllers/components/forms/simple_form_controller.rb
+++ app/controllers/components/forms/simple_form_controller.rb
@@ -4,6 +4,8 @@ class Components::Forms::SimpleFormController < Components::FormsController
     @user = User.new params[:user].nil? ? {} : inputs_params
   end
 
+  private
+
   def inputs_params
     params.require(:user).permit(:name, :country_id, :price, :company, :price_formula, email: [])
   end
--- app/controllers/components/navs_controller.rb
+++ app/controllers/components/navs_controller.rb
@@ -4,6 +4,13 @@ class Components::NavsController < ApplicationController
   end
 
   def navbar
+    @user = User.new params[:user].nil? ? {} : inputs_params
+  end
+
+  private
+
+  def inputs_params
+    params.require(:user).permit(:name, :country_id, :price, :company, :price_formula, email: [])
   end
 
 end
