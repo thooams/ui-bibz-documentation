07b26a2189ecf5b193a66ca2afeb2487592cde3a
Thomas HUMMEL <thomas@hummel.link>
2017-10-30 14:47:48 +0100

    remove ui bibz info gem in initializer file

--- config/initializers/app_info.rb
+++ config/initializers/app_info.rb
@@ -2,11 +2,11 @@ module UiBibzApp
   class Application
 
     UI_BIBZ     = Gem::Specification.find_by_name('ui_bibz')
-    VERSION     = UI_BIBZ.version.to_s
-    NAME        = UI_BIBZ.name.humanize.titleize
-    DESCRIPTION = UI_BIBZ.description
-    SUMMARY     = UI_BIBZ.summary
-    LICENSE     = UI_BIBZ.license
+    VERSION     = "2.0.0.beta2" # UI_BIBZ.version.to_s
+    NAME        = "Ui Bibz" # UI_BIBZ.name.humanize.titleize
+    DESCRIPTION = "A Rails Interface Framework using Bootstrap." # UI_BIBZ.description
+    SUMMARY     = "Create your project with Ui Bibz. Over a thirty reusable components built to provide iconography, dropdowns, input groups, navigation, alerts, and much more." # UI_BIBZ.summary
+    LICENSE     = "MIT" #UI_BIBZ.license
     STATUSES    = %i(primary secondary success danger warning info light dark)
     SIZES       = %i(lg md sm)
 
