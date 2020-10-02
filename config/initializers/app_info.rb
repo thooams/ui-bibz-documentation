80d751202fc2e6cd8b9db28c746b5628fc47e68d
Thomas HUMMEL <thomas@hummel.link>
2020-10-02 17:18:57 +0200

    Update col, row and container doc

--- config/initializers/app_info.rb
+++ config/initializers/app_info.rb
@@ -10,6 +10,7 @@ module UiBibzApp
     LICENSE     = UiBibz::LICENSE
     STATUSES    = UiBibz::Ui::Core::Component::STATUSES
     SIZES       = UiBibz::Ui::Core::Component::SIZES
+    BREAKPOINTS = UiBibz::Ui::Core::Component::BREAKPOINTS
     FONTAWESOME_VERSION = UiBibz::FONTAWESOME_VERSION
     BOOTSTRAP_VERSION   = UiBibz::BOOTSTRAP_VERSION
   end
