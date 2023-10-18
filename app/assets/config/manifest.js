b8396ee94370326b2d427fdb384b1bb4c7e74471
Thomas HUMMEL <thomas@hummel.link>
2023-10-18 13:27:14 +0200

    [...work in progress...]

--- app/assets/config/manifest.js
+++ app/assets/config/manifest.js
@@ -1,5 +1,6 @@
 //= link_tree ../images
 //= link_directory ../stylesheets .css
 //= link_tree ../../javascript .js
+//= link_tree ../../javascript/controllers .js
 //= link ui_bibz.js
 //= link rails-ujs.js
